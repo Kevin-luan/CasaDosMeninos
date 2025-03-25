80c0e5fce3e966827f9c7f8e3912aaea31a748ad
Kevin luan <cardosoluan3006@gmail.com>
2025-03-25 17:21:23 -0300

    Começando
    Teste

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CasaDosMeninos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
