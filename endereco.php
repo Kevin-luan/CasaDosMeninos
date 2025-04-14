577473a2add296a7519de7acfb30b4b52ff7fd7f
Kevin-luan <cardosoluan3006@gmail.com>
2025-04-14 13:27:28 -0300

    \xsa

--- endereco.php
+++ endereco.php
@@ -85,7 +85,7 @@ if(isset($_POST['submit'])){
         
 
         </form>
-        <a href="perguntas.html"><button>Voltar</button></a>
+        <a href="perguntas.php"><button>Voltar</button></a>
 
     </div>
     
