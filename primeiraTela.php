3cedca6177a4512da222e427c4f1be3a45613d69
Kevin luan <cardosoluan3006@gmail.com>
2025-05-20 22:27:51 -0300

    ajustando

--- primeiraTela.php
+++ primeiraTela.php
@@ -61,9 +61,7 @@ $result = $conexao->query(query: $sql);
                 echo "<td >".$user_data["nome"]."</td>";
                 echo "<td>".$user_data["cidade"]."</td>";
                 echo "<td>".$user_data["estado"]."</td>";
-                echo "<td><a href='perguntas.php?id=$user_data[id]'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-right-circle' viewBox='0 0 16 16'>
-            <path fill-rule='evenodd' d='M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z'/>
-          </svg></a></td>";
+                echo "<td><a href='perguntas.php?id=$user_data[id]'<div class='botao'> <button>Selecionar</button>  </div>   </a></td>";
                 
                 
                 
@@ -94,6 +92,7 @@ $result = $conexao->query(query: $sql);
     <div class="botao">
     <a href="login.php"><button>Novo endereço</button></a>
     </div>
+
 
 
    
